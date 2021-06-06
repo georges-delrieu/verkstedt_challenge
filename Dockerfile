a35adb99c264a977b887b948d3f26b26f2237df0
georges delrieu <georges.delrieu@sciencespo.fr>
2021-06-06 18:33:11 +0200

    finished initial setup

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,13 @@
-FROM nginx:latest
+# FROM nginx:latest
 
-COPY ./nginx/nginx.conf /etc/nginx/conf.d/default.conf
+# COPY ./landingpage/ /usr/share/nginx/html/
 
-COPY ./landingpage/ /usr/share/nginx/html/
+# EXPOSE 80
 
+
+FROM nginx:stable-alpine as app-runtime
+RUN rm -rf /etc/nginx/conf.d
+COPY ./nginx/default.conf /etc/nginx/conf.d/default.conf
+COPY /landingpage/ /usr/share/nginx/html
+EXPOSE 80
+CMD ["nginx", "-g", "daemon off;"]
--- main.tf
+++ main.tf
@@ -1,68 +1,175 @@
-locals {
-  vpc_id           = "vpc-5e8e4c36"
-  subnet_id        = "subnet-613b3b1a"
-  ssh_user         = "ubuntu"
-  key_name         = "devops"
-  private_key_path = "~/Downloads/devops.pem"
-}
-
-
 provider "aws" {
-  region = "eu-west-3"
+  region  = "eu-west-3" 
 }
 
-resource "aws_security_group" "nginx" {
-  name        = "nginx_access"
-  vpc_id      = local.vpc_id
+resource "aws_default_vpc" "default_vpc" {
+}
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+
+
+# Providing a reference to our default subnets
+resource "aws_default_subnet" "default_subnet_a" {
+  availability_zone = "eu-west-3a"
+}
+
+resource "aws_default_subnet" "default_subnet_b" {
+  availability_zone = "eu-west-3b"
+}
+
+resource "aws_default_subnet" "default_subnet_c" {
+  availability_zone = "eu-west-3c"
+}
+
+resource "aws_ecr_repository" "my_first_ecr_repo" {
+  name = "my-first-ecr-repo" 
+}
+
+resource "aws_ecs_cluster" "verkstedt_cluster" {
+  name = "verkstedt-cluster" 
+}
+
+
+resource "aws_ecs_task_definition" "my_first_task" {
+  family                   = "my-first-task" 
+  container_definitions    = <<DEFINITION
+  [
+    {
+      "name": "my-first-task",
+      "image": "${aws_ecr_repository.my_first_ecr_repo.repository_url}",
+      "essential": true,
+      "portMappings": [
+        {
+          "containerPort": 80,
+          "hostPort": 80
+        }
+      ],
+      "memory": 512,
+      "cpu": 256
+    }
+  ]
+  DEFINITION
+  requires_compatibilities = ["FARGATE"] 
+  network_mode             = "awsvpc"    
+  memory                   = 512         
+  cpu                      = 256         
+  execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
+}
+
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name               = "ecsTaskExecutionRole"
+  assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
   }
+}
 
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+  role       = "${aws_iam_role.ecsTaskExecutionRole.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_alb" "application_load_balancer" {
+  name               = "test-lb-tf" 
+  load_balancer_type = "application"
+  subnets = [
+    "${aws_default_subnet.default_subnet_a.id}",
+    "${aws_default_subnet.default_subnet_b.id}",
+    "${aws_default_subnet.default_subnet_c.id}"
+  ]
+  security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
+}
+
+# Security group for the load balancer:
+resource "aws_security_group" "load_balancer_security_group" {
   ingress {
-    from_port   = 80
+    from_port   = 80 
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] 
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 0 
+    to_port     = 0 
+    protocol    = "-1"  
+    cidr_blocks = ["0.0.0.0/0"] 
   }
 }
 
-resource "aws_instance" "nginx" {
-  ami                        = "ami-08d3d8790db280475"
-  subnet_id                  = local.subnet_id
-  instance_type              = "t2.micro"
-  associate_public_ip_address = true
-  security_groups            = [aws_security_group.nginx.id]
-  key_name                   = local.key_name
-
-  provisioner "remote-exec" {
-    inline = ["echo SSH getting ready"]
-
-    connection {
-      type        = "ssh"
-      user        = local.ssh_user
-      private_key = file(local.private_key_path)
-      host        = aws_instance.nginx.public_ip
-    }
-  }
-
-  provisioner "local-exec" {
-    command = "ansible-playbook -i ${aws_instance.nginx.public_ip}, --private-key ${local.private_key_path} nginx.yaml"
-
+resource "aws_lb_target_group" "target_group" {
+  name        = "target-group"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = "${aws_default_vpc.default_vpc.id}"
+  health_check {
+    matcher = "200,301,302"
+    path = "/"
   }
 }
 
-output "nginx_ip" {
-  value = aws_instance.nginx.public_ip
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = "${aws_alb.application_load_balancer.arn}"
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.target_group.arn}"
+  }
 }
+
+resource "aws_ecs_service" "my_first_service" {
+  name            = "my-first-service"                             
+  cluster         = "${aws_ecs_cluster.verkstedt_cluster.id}"             
+  task_definition = "${aws_ecs_task_definition.my_first_task.arn}" 
+  launch_type     = "FARGATE"
+  desired_count   = 3 
+
+  load_balancer {
+    target_group_arn = "${aws_lb_target_group.target_group.arn}" 
+    container_name   = "${aws_ecs_task_definition.my_first_task.family}"
+    container_port   = 80 
+  }
+
+  network_configuration {
+    subnets          = ["${aws_default_subnet.default_subnet_a.id}", "${aws_default_subnet.default_subnet_b.id}", "${aws_default_subnet.default_subnet_c.id}"]
+    assign_public_ip = true 
+    security_groups  = ["${aws_security_group.service_security_group.id}"] 
+    
+  }
+}
+
+resource "aws_security_group" "service_security_group" {
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
+  }
+
+  egress {
+    from_port   = 0 
+    to_port     = 0 
+    protocol    = "-1" 
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+}
+
+
+
+
+
+
+
+
+
+
 
